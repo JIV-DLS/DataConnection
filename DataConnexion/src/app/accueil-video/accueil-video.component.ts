fcd020a724a9cfa25e978cdf40a00fc1bc8dcca3
HONYIGLOH Jean Armand <55703251+RmannLePhilosopheFou@users.noreply.github.com>
2020-02-13 17:06:07 +0100

    Creation de la page affichage de video

---
+++ DataConnexion/src/app/accueil-video/accueil-video.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accueil-video',
+  templateUrl: './accueil-video.component.html',
+  styleUrls: ['./accueil-video.component.scss']
+})
+export class AccueilVideoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
